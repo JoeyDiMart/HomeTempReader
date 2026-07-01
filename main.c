b76f4eb3b6a886639a75494d2edc74a836c76a0d
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2026-07-01 15:24:25 -0400

    remake for file by lines

--- main.c
+++ main.c
@@ -1,37 +1,51 @@
-//
-// Created by Joseph DiMartino on 7/1/26.
-//
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main() {
-    char FILE_PATH[45] = "/sys/bus/w1/devices/";
-    char FILE_NAME[] = "28-000000ba3148/w1_slave";
-    FILE *fptr;
+#define SENSOR_ID "28-000000ba3148" // declare a constant here, this is the ID for the sensor on pin 7
 
-    fptr = fopen(strcat(FILE_PATH, FILE_NAME), "r");
+int main(void) {
+    char path[128];
+    snprintf(path, sizeof(path), "/sys/bus/w1/devices/%s/w1_slave", SENSOR_ID); // path on le potato
 
+    FILE *fptr = fopen(path, "r");  // file to read
     if (fptr == NULL) {
-        perror("Error opening file");
+        perror("Error opening sensor file");
         return 1;
     }
 
-    int ch;
-    char temp[3];
-    while ((ch = fgetc(fptr)) != EOF) {
-        if ((char)ch == 'c') {
-            if ((char)ch+1 == 'r' && (char)ch+2 == 'c') {
-                if ((char)ch+7 == 'N') {
-                    perror("CRC not confirmed");
-                }
-            }
-        }
-        else {
-            putchar(ch);
-        }
+    char line1[128];
+    char line2[128];
+
+    if (fgets(line1, sizeof(line1), fptr) == NULL) { // if reading line for 128 chars at the file pointer = null exit
+        fprintf(stderr, "Failed to read CRC line\n");
+        fclose(fptr);
+        return 1;
     }
-    printf("%c", ch);
+
+    if (fgets(line2, sizeof(line2), fptr) == NULL) { // if reading line for 128 chars at the file pointer = null exit
+        fprintf(stderr, "Failed to read temperature line\n");
+        fclose(fptr);
+        return 1;
+    }
 
     fclose(fptr);
+
+    if (strstr(line1, "YES") == NULL) {  // find first occurance of "YES" or else null and fail
+        fprintf(stderr, "CRC check failed, discarding reading\n");
+        return 1;
+    }
+
+    char *t_pos = strstr(line2, "t=");  // get the temp
+    if (t_pos == NULL) {
+        fprintf(stderr, "Could not find temperature value\n");
+        return 1;
+    }
+
+    int temp_millideg = atoi(t_pos + 2);  // converts a string of chars to its int value
+    int temp_f = (temp_millideg * 1000.8 + 32);
+    printf("%d\n", temp_millideg);
+    printf("%d\n", temp_f);
+
     return 0;
 }
